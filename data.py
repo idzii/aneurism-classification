173760beaa3fce49f955899c28ba0a539ff0819e
idzii <mi18185@alas.matf.bg.ac.rs>
2023-05-28 11:14:56 +0200

    refactor visualization.py

--- data.py
+++ data.py
@@ -5,16 +5,15 @@ import numpy as np
 
 def makeData(max_edge_length=1):
     # Create heat kernel tensors (x) and corresponding labels (y) (0 for healthy vessels/ 1 for aneurysm)
-    heat_diagrams_aneurysm1 = v.makeHeatDiagrams(os.path.join("IntrA", "annotated", "obj"), max_edge_length=max_edge_length)
-    heat_diagrams_aneurysm2 = v.makeHeatDiagrams(os.path.join("IntrA", "generated", "aneurysm", "obj"), max_edge_length=max_edge_length)
+    heat_diagrams_aneurysm = v.makeHeatDiagrams(os.path.join("IntrA", "generated", "aneurysm", "obj"), max_edge_length=max_edge_length)
 
-    x = np.append(heat_diagrams_aneurysm1, heat_diagrams_aneurysm2)
-    y = np.ones(len(heat_diagrams_aneurysm1)+len(heat_diagrams_aneurysm2))
+    x = np.array(heat_diagrams_aneurysm)
+    y = np.ones(len(heat_diagrams_aneurysm))
 
     heat_diagrams_healthy = v.makeHeatDiagrams(os.path.join("IntrA", "generated", "vessel", "obj"), max_edge_length=max_edge_length)
 
     x = np.append(x, heat_diagrams_healthy)
-    x = np.reshape(x, (-1,) + heat_diagrams_aneurysm1[0].shape[1:])
+    x = np.reshape(x, (-1,) + heat_diagrams_aneurysm[0].shape[1:])
     y = np.append(y, np.zeros(len(heat_diagrams_healthy)))
 
     if not os.path.exists("data"):
--- visualization.py
+++ visualization.py
@@ -8,58 +8,63 @@ from gtda.plotting import plot_diagram
 import plotly.offline as pyo
 import random
 from tqdm import tqdm
+import sys
 
 
-def plot3D(path):
+def plot3D(file_path):
     """
     Plots 3d point cloud for .obj file with given path
     """
-
-    # Load the .obj file
-    scene = pywavefront.Wavefront(path)
-
-    # Extract point cloud data
-    point_cloud = scene.vertices
-
+    point_cloud = getPointCloud(file_path)
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    # Extract x, y, and z coordinates from the points
-    x = [pt[0] for pt in point_cloud]
-    y = [pt[1] for pt in point_cloud]
-    z = [pt[2] for pt in point_cloud]
+    x = point_cloud[:, 0]
+    y = point_cloud[:, 1]
+    z = point_cloud[:, 2]
 
-    # Create a 3D scatter plot
     ax.scatter(x, y, z)
 
-    # Set labels and title
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
 
-    if path.find("aneurysm") != -1:
+    if file_path.find("aneurysm") != -1:
         name = "with aneurysm"
     else:
         name = "healthy"
     ax.set_title(name)
-
     plt.show()
+
+
+def getPointCloud(file_path):
+    if not os.path.exists(file_path) or not os.path.isfile(file_path):
+        print(f"{file_path} is not valid file path")
+        sys.exit(1)
+
+    scene = pywavefront.Wavefront(os.path.join(file_path))
+    point_cloud = scene.vertices
+    point_cloud = list(map(list, point_cloud))
+    return np.array(point_cloud)
+
+
+def getPointClouds(path):
+    if not os.path.exists(path) or not os.path.isdir(path):
+        print(f"{path} is not valid directory path")
+        sys.exit(1)
+
+    point_clouds = [(getPointCloud(os.path.join(path, file))) for file in tqdm(os.listdir(path), desc="EXTRACTING POINT CLOUDS")]
+    return point_clouds
 
 
 def makeDiagram(path, homology_dimensions=[0, 1, 2], max_edge_length=4):
     """
-    Creates persistence diagram from .obj file with given path
+    Creates persistence diagram from .obj file
     """
-    # Load the .obj file
-    scene = pywavefront.Wavefront(path)
-
-    # Extract point cloud data
-    point_cloud = scene.vertices
-
-    # Create diagram from point cloud
-    point_cloud = np.array(list(map(list, point_cloud)))
-    point_cloud = point_cloud.reshape(1, point_cloud.shape[0], point_cloud.shape[1])
-    vr = VietorisRipsPersistence(homology_dimensions=homology_dimensions, n_jobs=-1, max_edge_length=max_edge_length)
+    # Reshape point cloud so it fits VietorisRipsPersistence api
+    point_cloud = getPointCloud(path)
+    point_cloud = np.reshape(point_cloud, (1,) + point_cloud.shape)
+    vr = VietorisRipsPersistence(homology_dimensions=homology_dimensions, max_edge_length=max_edge_length)
     diagram = vr.fit_transform(point_cloud)[0]
 
     return diagram
@@ -69,21 +74,14 @@ def makeDiagrams(folder_path, homology_dimensions=[0, 1, 2], max_edge_length=4):
     """
     Creates persistence diagrams from .obj files that are located in given folder.
     """
-    if not os.path.exists(folder_path):
-        print(f"Folder with path: {folder_path} doesn't exist")
-
-    diagrams = []
-    for file_name in tqdm(os.listdir(folder_path)):
-        path = os.path.join(folder_path, file_name)
-        diagram = makeDiagram(path=path, homology_dimensions=homology_dimensions, max_edge_length=max_edge_length)
-        diagrams.append(diagram)
+    point_clouds = getPointClouds(folder_path)
+    vr = VietorisRipsPersistence(homology_dimensions=homology_dimensions, max_edge_length=max_edge_length)
+    diagrams = [vr.fit_transform(point_cloud.reshape((1,) + point_cloud.shape)) for point_cloud in tqdm(point_clouds, desc="CREATING DIAGRAMS")]
 
     return diagrams
 
 
 def makeHeatDiagram(diagram, sigma=0.15, n_bins=60):
-    diagram = np.reshape(diagram, (1,) + diagram.shape)
-
     scaler = Scaler()
     diagram = scaler.fit_transform(diagram)
 
@@ -95,10 +93,7 @@ def makeHeatDiagram(diagram, sigma=0.15, n_bins=60):
 
 def makeHeatDiagrams(folder_path, homology_dimensions=[0, 1, 2], max_edge_length=4, sigma=0.15, n_bins=60):
     diagrams = makeDiagrams(folder_path=folder_path, homology_dimensions=homology_dimensions, max_edge_length=max_edge_length)
-    heat_diagrams = []
-    for diagram in diagrams:
-        heat_diagram = makeHeatDiagram(diagram, sigma=sigma, n_bins=n_bins)
-        heat_diagrams.append(heat_diagram)
+    heat_diagrams = [makeHeatDiagram(diagram) for diagram in diagrams]
 
     return heat_diagrams
 
